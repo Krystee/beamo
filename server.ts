76c42df6017f8ca346164aab803ba6d27d9a826f
Krystee <Krysteem@gmail.com>
2024-11-15 21:18:00 +0000

    updated netlify, package, pnpm and server to fix config issues

--- server.ts
+++ server.ts
@@ -2,6 +2,10 @@ import { createRequestHandler } from "@remix-run/netlify";
 import * as build from "@remix-run/dev/server-build";
 
 export const handler = createRequestHandler({
+  // @ts-expect-error - Intentional version mismatch between Remix 1.x and 2.x
   build,
   mode: process.env.NODE_ENV,
+  getLoadContext() {
+    return {};
+  },
 });

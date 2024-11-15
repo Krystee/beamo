8db4c654435eb2bec7d11e6a1f364d2028a78fd5
Krystee <Krysteem@gmail.com>
2024-11-15 21:34:53 +0000

    added css

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,14 +1,26 @@
-var __defProp = Object.defineProperty;
-var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: !0 });
-};
-
 // netlify/functions/server.js
 import { createRequestHandler } from "@remix-run/netlify";
-
-// server-entry-module:@remix-run/dev/server-build
-var server_build_exports = {};
+import { PassThrough } from "node:stream";
+import { createReadableStreamFromReadable } from "@remix-run/node";
+import { RemixServer } from "@remix-run/react";
+import { isbot } from "isbot";
+import { renderToPipeableStream } from "react-dom/server";
+import { jsx } from "react/jsx-runtime";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration
+} from "@remix-run/react";
+import { jsx as jsx2, jsxs } from "react/jsx-runtime";
+import { jsx as jsx3, jsxs as jsxs2 } from "react/jsx-runtime";
+import { jsx as jsx4 } from "react/jsx-runtime";
+var __defProp = Object.defineProperty, __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: !0 });
+}, server_build_exports = {};
 __export(server_build_exports, {
   assets: () => assets_manifest_default,
   assetsBuildDirectory: () => assetsBuildDirectory,
@@ -18,18 +30,10 @@ __export(server_build_exports, {
   publicPath: () => publicPath,
   routes: () => routes
 });
-
-// app/entry.server.tsx
 var entry_server_exports = {};
 __export(entry_server_exports, {
   default: () => handleRequest
 });
-import { PassThrough } from "node:stream";
-import { createReadableStreamFromReadable } from "@remix-run/node";
-import { RemixServer } from "@remix-run/react";
-import { isbot } from "isbot";
-import { renderToPipeableStream } from "react-dom/server";
-import { jsx } from "react/jsx-runtime";
 var ABORT_DELAY = 5e3;
 function handleRequest(request, responseStatusCode, responseHeaders, remixContext, loadContext) {
   return isbot(request.headers.get("user-agent") || "") ? handleBotRequest(
@@ -110,25 +114,12 @@ function handleBrowserRequest(request, responseStatusCode, responseHeaders, remi
     setTimeout(abort, ABORT_DELAY);
   });
 }
-
-// app/root.tsx
 var root_exports = {};
 __export(root_exports, {
   default: () => App,
   links: () => links
 });
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration
-} from "@remix-run/react";
-import { jsx as jsx2, jsxs } from "react/jsx-runtime";
-var links = () => [
-  ...void 0 ? [{ rel: "stylesheet", href: void 0 }] : []
-];
+var links = () => [];
 function App() {
   return /* @__PURE__ */ jsxs("html", { lang: "en", children: [
     /* @__PURE__ */ jsxs("head", { children: [
@@ -145,16 +136,11 @@ function App() {
     ] })
   ] });
 }
-
-// app/routes/_index.tsx
 var index_exports = {};
 __export(index_exports, {
   default: () => Index,
   meta: () => meta
 });
-
-// app/components/CapitalGainsCard.tsx
-import { jsx as jsx3, jsxs as jsxs2 } from "react/jsx-runtime";
 var CapitalGainsCard = () => /* @__PURE__ */ jsx3("div", { className: "bg-black p-4 lg:p-8", children: /* @__PURE__ */ jsxs2("div", { className: "w-full px-4 lg:px-6 py-2 bg-[#161618] rounded-[10px] border border-[#2C2C30] flex flex-col lg:inline-flex lg:flex-row items-center gap-4 lg:gap-6 font-['Montserrat']", children: [
   /* @__PURE__ */ jsxs2("div", { className: "w-full lg:flex-1 flex flex-col gap-3.5", children: [
     /* @__PURE__ */ jsxs2("div", { className: "flex items-center gap-2.5", children: [
@@ -193,23 +179,14 @@ var CapitalGainsCard = () => /* @__PURE__ */ jsx3("div", { className: "bg-black 
       ] }) })
     ] })
   ] })
-] }) }), CapitalGainsCard_default = CapitalGainsCard;
-
-// app/routes/_index.tsx
-import { jsx as jsx4 } from "react/jsx-runtime";
-var meta = () => [
+] }) }), CapitalGainsCard_default = CapitalGainsCard, meta = () => [
   { title: "Capital Gains Dashboard" },
   { name: "description", content: "Capital Gains Dashboard" }
 ];
 function Index() {
   return /* @__PURE__ */ jsx4("div", { className: "min-h-screen bg-black", children: /* @__PURE__ */ jsx4(CapitalGainsCard_default, {}) });
 }
-
-// server-assets-manifest:@remix-run/dev/assets-manifest
-var assets_manifest_default = { entry: { module: "/build/entry.client-7GFXHO4N.js", imports: ["/build/_shared/chunk-ACO73CVB.js", "/build/_shared/chunk-43PMWAD2.js"] }, routes: { root: { id: "root", parentId: void 0, path: "", index: void 0, caseSensitive: void 0, module: "/build/root-QJCQ4UGS.js", imports: void 0, hasAction: !1, hasLoader: !1, hasClientAction: !1, hasClientLoader: !1, hasErrorBoundary: !1 }, "routes/_index": { id: "routes/_index", parentId: "root", path: void 0, index: !0, caseSensitive: void 0, module: "/build/routes/_index-6QJEUBZS.js", imports: void 0, hasAction: !1, hasLoader: !1, hasClientAction: !1, hasClientLoader: !1, hasErrorBoundary: !1 } }, version: "63646b33", hmr: void 0, url: "/build/manifest-63646B33.js" };
-
-// server-entry-module:@remix-run/dev/server-build
-var mode = "production", assetsBuildDirectory = "public/build", future = { v3_fetcherPersist: !1, v3_relativeSplatPath: !1, v3_throwAbortReason: !1, v3_singleFetch: !1, v3_lazyRouteDiscovery: !1, unstable_optimizeDeps: !1, unstable_routeConfig: !1 }, publicPath = "/build/", entry = { module: entry_server_exports }, routes = {
+var assets_manifest_default = { entry: { module: "/build/entry.client-7GFXHO4N.js", imports: ["/build/_shared/chunk-ACO73CVB.js", "/build/_shared/chunk-43PMWAD2.js"] }, routes: { root: { id: "root", parentId: void 0, path: "", index: void 0, caseSensitive: void 0, module: "/build/root-QJCQ4UGS.js", imports: void 0, hasAction: !1, hasLoader: !1, hasClientAction: !1, hasClientLoader: !1, hasErrorBoundary: !1 }, "routes/_index": { id: "routes/_index", parentId: "root", path: void 0, index: !0, caseSensitive: void 0, module: "/build/routes/_index-6QJEUBZS.js", imports: void 0, hasAction: !1, hasLoader: !1, hasClientAction: !1, hasClientLoader: !1, hasErrorBoundary: !1 } }, version: "63646b33", hmr: void 0, url: "/build/manifest-63646B33.js" }, mode = "production", assetsBuildDirectory = "public/build", future = { v3_fetcherPersist: !1, v3_relativeSplatPath: !1, v3_throwAbortReason: !1, v3_singleFetch: !1, v3_lazyRouteDiscovery: !1, unstable_optimizeDeps: !1, unstable_routeConfig: !1 }, publicPath = "/build/", entry = { module: entry_server_exports }, routes = {
   root: {
     id: "root",
     parentId: void 0,
@@ -226,10 +203,7 @@ var mode = "production", assetsBuildDirectory = "public/build", future = { v3_fe
     caseSensitive: void 0,
     module: index_exports
   }
-};
-
-// netlify/functions/server.js
-var handler = createRequestHandler({
+}, handler = createRequestHandler({
   // @ts-expect-error - Intentional version mismatch between Remix 1.x and 2.x
   build: server_build_exports,
   mode: "production",

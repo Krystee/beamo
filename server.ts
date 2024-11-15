25f61974fde13deb3ce6040bee06c890898ec2df
Krystee <Krysteem@gmail.com>
2024-11-15 21:23:03 +0000

    more attempts to fix config

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
 import { createRequestHandler } from "@remix-run/netlify";
-import * as build from "@remix-run/dev/server-build";
+import * as remixBuild from "@remix-run/dev/server-build";
 
 export const handler = createRequestHandler({
   // @ts-expect-error - Intentional version mismatch between Remix 1.x and 2.x
-  build,
+  build: remixBuild,
   mode: process.env.NODE_ENV,
   getLoadContext() {
     return {};
